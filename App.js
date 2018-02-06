62455c9262166f3e1b9692de46d13018dbad5c9e
Gavin Brown <scareb15@gmail.com>
2018-02-05 21:55:41 -0700

    starter files

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Landing from './components/Landing'
+import { Font } from 'expo'
+
+
+export default class App extends React.Component {
+  state ={
+    fontLoaded: false,
+    homeScreen:false
+  }
+ async componentDidMount() {
+    await Font.loadAsync({
+      Mono: require('./assets/fonts/Monofett.ttf'),
+      Open: require('./assets/fonts/OpenSans-SemiBoldItalic.ttf')
+    })
+    this.setState({fontLoaded:true})
+  }
+
+  _homescreen = () => {
+    this.setState({fontLoaded: false, homeScreen: true})
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.fontLoaded ? <Landing _homescreen={this._homescreen}/> : null}
+        {this.state.homeScreen}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
---
+++ components/Landing.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { View, StyleSheet, Text, Dimensions, ImageBackground } from 'react-native'
+import {Button, Icon} from 'native-base'
+
+let { width, height } = Dimensions.get('window')
+
+class Landing extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.title}>r3phr@$e</Text>
+                    <Text style={styles.subtitle}>Let's clean it up</Text>
+                </View>
+
+                <View>
+                    <Button iconLeft success small onPress={this.props._homescreen} style={{padding: 10, marginTop: 20}}><Icon name='happy'/><Text style={styles.buttonSpacing}>Enter</Text></Button>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 70,
+        fontFamily: 'Mono',
+        margin: 10
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontFamily: 'Open',
+        fontSize: 20
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: height,
+    },
+    header: {
+        justifyContent: 'space-between',
+       
+    },
+    buttonSpacing:{
+        margin:10,
+        fontSize: 16,
+        color: 'white'
+    }
+})
+
+export default Landing
