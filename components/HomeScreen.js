07966ed26c51b463c77be4d63cd3f5572f179b68
Ellis Mwekunda <Scareb15@gmail.com>
2018-02-07 13:31:23 -0700

    added activate camera component to homescreen

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,54 +1,100 @@
 import React from 'react'
 import {View, Text} from 'react-native'
-import { StackNavigator} from 'react-navigation'
+import { StackNavigator, TabNavigator, TabBarBottom, tabBarIcon} from 'react-navigation'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import Funny from './Funny'
+import Camera from './CameraAc'
 import {Button, Icon} from 'native-base'
+import Loving from './Loving'
 
 class HomeScreen extends React.Component{
-    constructor(props){
-        super(props)
-        this.state ={
-            camera: false
-        }
+    state ={
+        camera: false,
+        home: true
     }
-    
-    static navigationOptions = {
-        title: 'Home',
-        headerTitleStyle: {
-            fontFamily: 'Open',
-            fontSize: 20
-        },
-        headerBackTitle: null,
-        headerRight: (
-            
-            <Button transparent
-              onPress={() => alert('This is a button!')} style={{marginRight: 10}}>
-              <Icon name='paper-plane' style={{color: 'black', margin: 10}}/>
-              </Button>
-            
-        ),
-        headerLeft: (
-            <Button  transparent
-              onPress={() => alert('clicked')} style={{marginLeft: 10}}>
-              <Icon name='camera' style={{color: 'black', margin: 10}}/>
-              </Button>
-        )
+    componentWillMount(){
+        this.props.navigation.setParams({_cameraActive:this._cameraActive})
+    }
+
+    _cameraActive = () => {
+        this.setState({camera: !this.state.camera, home: !this.state.home})
+      }
+    static navigationOptions = ({navigation}) => {
+        const params = navigation.state.params || {};
+        return {
+            title: 'Home',
+            headerTitleStyle: {
+                fontFamily: 'Mono',
+                fontSize: 36
+            },
+            headerBackTitle: null,
+            headerRight: (
+                
+                <Button transparent
+                  onPress={() => alert('This is a button!')} style={{marginRight: 10}}>
+                  <Icon name='paper-plane' style={{color: 'black', margin: 10}}/>
+                  </Button>
+                
+            ),
+            headerLeft: (
+                <Button  transparent
+                  onPress={params._cameraActive} style={{marginLeft: 8}}>
+                  <Icon name='camera' style={{color: 'black', fontSize: 30}}/>
+                  </Button>
+            )
+        }
     }
     render(){
         return (
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>HomeScreen</Text>
-                <Button title='Funny' onPress={() => this.props.navigation.navigate('Funny')}/>
+            <View style={{flex: 1}}>
+                {this.state.home ? (<View><Text>HomeScreen</Text>
+                <Button title='Funny' onPress={() => this.props.navigation.navigate('Funny')}/></View>) : null}
+                {this.state.camera?(<Camera />): null}
+                
             </View>
         )
     }
 }
 
-export default StackNavigator({
-    Home: {
-        screen: HomeScreen,
-    },
-    Funny: {
-        screen: Funny
-    }
+const HomeStack = StackNavigator({
+    Home:{screen:HomeScreen}
 })
+const FunnyStack = StackNavigator({
+    Funny:{screen: Funny}
+})
+const LovingStack = StackNavigator({
+    Loving:{screen: Loving}
+})
+
+export default TabNavigator (
+    {
+      Home: { screen: HomeStack },
+      Funny: { screen: FunnyStack },
+      Loving:{screen: LovingStack}
+    },
+    {
+      navigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, tintColor }) => {
+          const { routeName } = navigation.state;
+          let iconName;
+          if (routeName === 'Home') {
+            iconName = `ios-home${focused ? '' : '-outline'}`;
+          } else if (routeName === 'Funny') {
+            iconName = `ios-happy${focused ? '' : '-outline'}`;
+          } else if(routeName === 'Loving'){
+              iconName = `ios-heart${focused ? '' : '-outline'}`
+          }
+          return <Ionicons name={iconName} size={25} color={tintColor} />;
+        },
+      }),
+      tabBarComponent: TabBarBottom,
+      tabBarPosition: 'bottom',
+      tabBarOptions: {
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      },
+      animationEnabled: false,
+      swipeEnabled: false,
+    }
+  );
+  
