52d5ef748fe3d7020af50414dfb0a60a6d2f8c0c
Gavin Brown <scareb15@gmail.com>
2018-02-07 20:29:09 -0700

    added serious component

---
+++ components/Serious.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import { StackNavigator} from 'react-navigation'
+
+export default class Serious extends React.Component {
+    static navigationOptions = {
+        title: 'r3phr@$e',
+        headerStyle:{
+            backgroundColor: "rgb(112, 176, 153)"
+          },
+        headerTitleStyle:{
+            fontFamily: 'Pacifico',
+            fontSize: 28,
+            color:'white',
+            marginBottom: 17,
+            padding: 5
+        }
+    }
+    render(){
+        return (
+            <View style={{flex: 1,backgroundColor: "rgb(233, 233,240)"}}>
+                <Text>Serious</Text>
+            </View>
+        )
+    }
+}
