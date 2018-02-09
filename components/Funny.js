87597aad6fdcc650b568ceeed3c712433c7c6811
Ellis Mwekunda <Scareb15@gmail.com>
2018-02-09 10:11:51 -0700

    added text input

--- components/Funny.js
+++ components/Funny.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 import { StackNavigator} from 'react-navigation'
+import { TextField} from 'react-native-material-textfield'
 
 export default class Funny extends React.Component {
+    state ={
+        value: ''
+    }
+
     static navigationOptions = {
         title: 'r3phr@$e',
         headerStyle:{
@@ -17,11 +22,12 @@ export default class Funny extends React.Component {
         }
     }
     render(){
+        let value = this.state.value
         return (
             <View style={{flex: 1, backgroundColor: "rgb(233, 233,240)"}}>
             <Text style={styles.header}>Funny</Text>
             <View style={{flex: 1, margin: 10, backgroundColor: 'white', borderRadius: 1.5, borderWidth: 0.16, borderColor: 'black', shadowColor: 'black', shadowOffset:{width: 0 , height: 2}, shadowOpacity: 0.2}}>
-                
+                <TextField label='Mean Text' value={value} onChangeText={(e) => this.setState({value: e})} multiline={true}/>
               </View>
               <View style={{flex: 1, margin: 10, backgroundColor: 'white', borderRadius: 1.5, borderWidth: 0.16, borderColor: 'black', shadowColor: 'black', shadowOffset:{width: 0 , height: 2}, shadowOpacity: 0.2}}>
                  
